de60a8b973a5ca0c0d526401f7964819ec6337af
hieu2211 <h.vanhieu1999@gmail.com>
2020-05-21 16:17:09 +0700

    hiue

--- Models/IndexViewModel.cs
+++ Models/IndexViewModel.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 
 namespace netCore.Models
 {
-    public class IndexSlideModel
+    public class IndexModelHome
     {
         public string RequestId { get; set; }
         public List<ClassImage> Image{ get; set; }
-        // public List<ClassImage> Image{ get; set; }
+        
+        public List<ClassForLevel> ForLevels{get;set;}
     }
     public class ClassImage{
          public string ImageName{ get; set; }
+    }
+    public class ClassForLevel{
+         public string ImageName{ get; set; }
+         public string NameFL{ get; set; }
+         public string Icon{ get; set; }
     }
 }
